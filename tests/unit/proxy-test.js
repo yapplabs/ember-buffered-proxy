e01c9647b07303f663c8c5a77175b0281c22a713
Travis Hoover <thoov@nestlabs.com>
2016-05-25 00:31:14 -0700

    Upgrade to ember-cli 2.5 and update to ES2015

--- tests/unit/proxy-test.js
+++ tests/unit/proxy-test.js
@@ -1,4 +1,3 @@
-import Ember from 'ember';
 import BufferedProxy from 'ember-buffered-proxy/proxy';
 import BufferedMixin from 'ember-buffered-proxy/mixin';
 import {
@@ -6,13 +5,12 @@ import {
   test
 } from 'qunit';
 
-
 module('ember-buffered-proxy/mixin');
 
-test('exists', function(assert) {
+test('exists', (assert) => {
   assert.ok(BufferedProxy);
 });
 
-test('that appears correct', function(assert) {
+test('that appears correct', (assert) => {
   assert.ok(BufferedMixin.detect(BufferedProxy.create()));
 });
