7d5c445653687182c9f886b5a1ed169a0608ad9d
Tomasz Nieżurawski <tommaqs@gmail.com>
2019-11-22 12:56:27 +0100

    Codemod: ember-test-helpers-codemod

--- tests/acceptance/application-test.js
+++ tests/acceptance/application-test.js
@@ -1,84 +1,76 @@
-import { test } from 'qunit';
-import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { click, fillIn, currentURL, find, visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
 
-moduleForAcceptance('Acceptance | application');
+module('Acceptance | application', function(hooks) {
+  setupApplicationTest(hooks);
 
-test('verify if changes on whole content object were applied', function(assert) {
-  visit('/');
+  test('verify if changes on whole content object were applied', async function(assert) {
+    await visit('/');
 
-  andThen(function() {
     assert.equal(currentURL(), '/');
 
     // Init: Buffer's and content's property value are the same
-    assert.equal(find('#buffer-firstname').text(), 'stefan');
-    assert.equal(find('#user-firstname').text(), 'stefan');
-    assert.equal(find('#buffer-email').text(), 'example@example.com');
-    assert.equal(find('#user-email').text(), 'example@example.com');
-    assert.equal(find('#buffer-has-changes').text(), 'false');
+    assert.equal(find('#buffer-firstname').textContent, 'stefan');
+    assert.equal(find('#user-firstname').textContent, 'stefan');
+    assert.equal(find('#buffer-email').textContent, 'example@example.com');
+    assert.equal(find('#user-email').textContent, 'example@example.com');
+    assert.equal(find('#buffer-has-changes').textContent, 'false');
 
     // Change buffer value: there should be a difference between buffer and content
-    fillIn('#firstname-input', 'tomek');
-    fillIn('#email-input', 'test@dot.com');
-    andThen(() => {
-      assert.equal(find('#buffer-firstname').text(), 'tomek');
-      assert.equal(find('#user-firstname').text(), 'stefan');
-      assert.equal(find('#buffer-email').text(), 'test@dot.com');
-      assert.equal(find('#user-email').text(), 'example@example.com');
-      assert.equal(find('#buffer-has-changes').text(), 'true');
+    await fillIn('#firstname-input', 'tomek');
+    await fillIn('#email-input', 'test@dot.com');
+    assert.equal(find('#buffer-firstname').textContent, 'tomek');
+    assert.equal(find('#user-firstname').textContent, 'stefan');
+    assert.equal(find('#buffer-email').textContent, 'test@dot.com');
+    assert.equal(find('#user-email').textContent, 'example@example.com');
+    assert.equal(find('#buffer-has-changes').textContent, 'true');
 
-      // Apply buffer changes and verify if content has new values
-      click('button#apply-changes').then(() => {
-        assert.equal(find('#buffer-firstname').text(), 'tomek');
-        assert.equal(find('#user-firstname').text(), 'tomek');
-        assert.equal(find('#buffer-email').text(), 'test@dot.com');
-        assert.equal(find('#user-email').text(), 'test@dot.com');
-        assert.equal(find('#buffer-has-changes').text(), 'false');
-      });
-    });
+    // Apply buffer changes and verify if content has new values
+    await click('button#apply-changes');
+    assert.equal(find('#buffer-firstname').textContent, 'tomek');
+    assert.equal(find('#user-firstname').textContent, 'tomek');
+    assert.equal(find('#buffer-email').textContent, 'test@dot.com');
+    assert.equal(find('#user-email').textContent, 'test@dot.com');
+    assert.equal(find('#buffer-has-changes').textContent, 'false');
   });
-});
 
-test('verify if partial changes were applied', function(assert) {
-  visit('/');
+  test('verify if partial changes were applied', async function(assert) {
+    await visit('/');
 
-  andThen(function() {
     assert.equal(currentURL(), '/');
 
     // Init: Buffer's and content's property value are the same
-    assert.equal(find('#buffer-firstname').text(), 'stefan');
-    assert.equal(find('#user-firstname').text(), 'stefan');
-    assert.equal(find('#buffer-email').text(), 'example@example.com');
-    assert.equal(find('#user-email').text(), 'example@example.com');
-    assert.equal(find('#buffer-has-changes').text(), 'false');
+    assert.equal(find('#buffer-firstname').textContent, 'stefan');
+    assert.equal(find('#user-firstname').textContent, 'stefan');
+    assert.equal(find('#buffer-email').textContent, 'example@example.com');
+    assert.equal(find('#user-email').textContent, 'example@example.com');
+    assert.equal(find('#buffer-has-changes').textContent, 'false');
 
     // Change buffer value: there should be a difference between buffer and content
-    fillIn('#firstname-input', 'tomek');
-    fillIn('#email-input', 'test@dot.com');
-    andThen(() => {
-      assert.equal(find('#buffer-firstname').text(), 'tomek');
-      assert.equal(find('#user-firstname').text(), 'stefan');
-      assert.equal(find('#buffer-email').text(), 'test@dot.com');
-      assert.equal(find('#user-email').text(), 'example@example.com');
-      assert.equal(find('#buffer-has-changes').text(), 'true');
+    await fillIn('#firstname-input', 'tomek');
+    await fillIn('#email-input', 'test@dot.com');
+    assert.equal(find('#buffer-firstname').textContent, 'tomek');
+    assert.equal(find('#user-firstname').textContent, 'stefan');
+    assert.equal(find('#buffer-email').textContent, 'test@dot.com');
+    assert.equal(find('#user-email').textContent, 'example@example.com');
+    assert.equal(find('#buffer-has-changes').textContent, 'true');
 
-      // Apply buffer changes and verify if content has new values
-      click('button#apply-partial-changes').then(() => {
-        assert.equal(find('#buffer-firstname').text(), 'tomek');
-        assert.equal(find('#user-firstname').text(), 'stefan');
-        assert.equal(find('#buffer-email').text(), 'test@dot.com');
-        assert.equal(find('#user-email').text(), 'test@dot.com');
-        assert.equal(find('#buffer-has-changes').text(), 'true', 'still has changes to apply');
+    // Apply buffer changes and verify if content has new values
+    await click('button#apply-partial-changes');
+    assert.equal(find('#buffer-firstname').textContent, 'tomek');
+    assert.equal(find('#user-firstname').textContent, 'stefan');
+    assert.equal(find('#buffer-email').textContent, 'test@dot.com');
+    assert.equal(find('#user-email').textContent, 'test@dot.com');
+    assert.equal(find('#buffer-has-changes').textContent, 'true', 'still has changes to apply');
 
-        // Discard changes - we expect firstname to be back to original value
-        // But the change that was already applied, should remain
-        click('button#discard-changes').then(() => {
-          assert.equal(find('#buffer-firstname').text(), 'stefan');
-          assert.equal(find('#user-firstname').text(), 'stefan');
-          assert.equal(find('#buffer-email').text(), 'test@dot.com');
-          assert.equal(find('#user-email').text(), 'test@dot.com');
-          assert.equal(find('#buffer-has-changes').text(), 'false');
-        });
-      });
-    });
+    // Discard changes - we expect firstname to be back to original value
+    // But the change that was already applied, should remain
+    await click('button#discard-changes');
+    assert.equal(find('#buffer-firstname').textContent, 'stefan');
+    assert.equal(find('#user-firstname').textContent, 'stefan');
+    assert.equal(find('#buffer-email').textContent, 'test@dot.com');
+    assert.equal(find('#user-email').textContent, 'test@dot.com');
+    assert.equal(find('#buffer-has-changes').textContent, 'false');
   });
 });
