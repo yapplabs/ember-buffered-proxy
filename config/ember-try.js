318a085a2bb1d693f9cad72d533644bd48ededa1
Manuel Wiedenmann <manuel@funkensturm.de>
2022-05-11 14:27:04 +0200

    Cover ember versions starting from 3.13

--- config/ember-try.js
+++ config/ember-try.js
@@ -8,18 +8,10 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.8',
+        name: 'ember-3.13',
         npm: {
           devDependencies: {
-            'ember-source': '~3.8.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.12',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.12.0',
+            'ember-source': '~3.13.0',
           },
         },
       },
